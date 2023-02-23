b5ca786d5670160227eb8b864f145be1805f19b6
omar saadeh <omarsaadeh11@gmail.com>
2023-02-23 08:04:11 -0800

    building app css

--- src/shared/components/Footer/Footer.js
+++ src/shared/components/Footer/Footer.js
@@ -1,9 +1,9 @@
-import './Footer.css';
+import "./Footer.css";
 
 const Footer = () => {
   return (
-    <footer className='footer-section center-text'>
-      © 2022 Howie Wang. All rights reserved.
+    <footer className="footer-section center-text">
+      © 2023 Omar Saade. All rights reserved.
     </footer>
   );
 };
