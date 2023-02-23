bccdf46306489393a377b89eae98d73da80fe0c7
omar saadeh <omarsaadeh11@gmail.com>
2023-02-23 08:21:53 -0800

    building app css

--- src/shared/components/CallToAction/CallToAction.js
+++ src/shared/components/CallToAction/CallToAction.js
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 
 import Button from "../FormElements/Button";
+
 import { AuthContext } from "../../context/auth-context";
 
 import "./CallToAction.css";
@@ -13,8 +14,8 @@ const CallToAction = () => {
   }
 
   return (
-    <div className="CTA-section center-flex-column extra-small-gap">
-      <p className="CTA-section--text">Join us. Start your journey here.</p>
+    <div className="firstsection center-flex-column extra-small-gap">
+      <p className="firstsectiontext">Join us. Start your journey here.</p>
       <Button to="/auth" type="button" size="medium" primary inverse>
         Get Started
       </Button>
