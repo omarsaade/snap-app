deaa032fc2125a6894b66efdcf3591cbee748ed1
omar saadeh <omarsaadeh11@gmail.com>
2023-02-25 10:59:47 -0800

    building app

--- build/static/js/7.38102422.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpfrontend=this.webpackJsonpfrontend||[]).push([[7],{82:function(n,o){}}]);
-//# sourceMappingURL=7.38102422.chunk.js.map
--- build/static/js/8.f032766e.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpfrontend=this.webpackJsonpfrontend||[]).push([[8],{83:function(n,o){}}]);
-//# sourceMappingURL=8.f032766e.chunk.js.map
